efe3cb518204fbe76e64bbb20e256b5be0f06d94
Yuri Colen <yuri.colen@TODES003-NT.indiana.local>
2022-12-22 12:07:23 -0300

    Subindo tudo buildado da branch Annotations

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ParkingControlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingControlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingControlApplication.class, args);
+        System.out.println("teste");
+    }
 
 }
